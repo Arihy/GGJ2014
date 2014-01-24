ce238405f99503c79956595f3533ffeebf481bb8
Arihy <ynallem@gmail.com>
2014-01-24 23:30:40 +0100

    wip player

---
+++ GGJ14/Assets/Scriptes/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Transform _transform;
+	private float _speed;
+
+	// Use this for initialization
+	void Start () {
+		_transform = transform;
+		_speed = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+	}
+}
