6dc3862ed4f5563fa21daffb7c7bf65b1779b0e1
Arihy <ynallem@gmail.com>
2014-01-26 09:59:37 +0100

    wip observer

---
+++ GGJ14/Assets/Scriptes/Observer/MessageMgr.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//message qu'on peut envoyer
+public enum eMessageID
+{
+    eBut
+}
+
+public interface IMessageListener
+{
+    void OnMessage(eMessageID _messageID, GameObject _sender);
+}
+
+public class MessageMgr
+{
+    static MessageMgr m_Instance = null;
+
+    public static MessageMgr Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = new MessageMgr();
+            }
+            return m_Instance;
+        }
+    }
+
+    protected List<IMessageListener> m_listeners = new List<IMessageListener>(10);
+
+    public void AddListener(IMessageListener _listener)
+    {
+        m_listeners.Add(_listener);
+    }
+
+    public void RemoveListener(IMessageListener _listener)
+    {
+        m_listeners.Remove(_listener);
+    }
+
+    public void NotifyObservers(eMessageID _messageID, GameObject _sender)
+    {
+        foreach (IMessageListener observers in m_listeners)
+        {
+            observers.OnMessage(_messageID, _sender);
+        }
+    }
+}
